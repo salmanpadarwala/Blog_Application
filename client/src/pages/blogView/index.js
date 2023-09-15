340f7d1ba4f143aa7f04bb4b8284700807848f0a
Ahmad <ahmadpadarwala4@gmail.com>
2023-09-15 17:50:19 +0500

    News View Page design

---
+++ client/src/pages/blogView/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ViewNews from "./ViewNews";
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
+import '../../assets/css/pages/viewNews.css'
+
+const index = () => {
+  return (
+    <>
+      <Navbar />
+      <ViewNews />
+      <Footer />
+    </>
+  );
+};
+
+export default index;
